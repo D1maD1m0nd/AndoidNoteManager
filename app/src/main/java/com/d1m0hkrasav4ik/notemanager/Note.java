6577a09fb79abebce3957a9b5db7225e50152f07
D1m0nd <70190542+D1maD1m0nd@users.noreply.github.com>
2021-05-18 19:09:15 +0300

    Task#6 Добавление ресурсов Создание основ для фрагментов Добавление класса заметки

---
+++ app/src/main/java/com/d1m0hkrasav4ik/notemanager/Note.java
@@ -0,0 +1,9 @@
+package com.d1m0hkrasav4ik.notemanager;
+
+import java.util.Calendar;
+
+public class Note {
+    private String name;
+    private String description;
+    private Calendar date;
+}
