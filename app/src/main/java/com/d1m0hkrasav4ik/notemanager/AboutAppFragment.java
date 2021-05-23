58e422e7e1da6091a3a9741ba2de0d67776595b6
D1m0nd <70190542+D1maD1m0nd@users.noreply.github.com>
2021-05-24 01:18:12 +0300

    Task#7:сделал шторку меню, сделал добавление фрагмента заметок не через лэйаут

--- app/src/main/java/com/d1m0hkrasav4ik/notemanager/AboutAppFragment.java
+++ app/src/main/java/com/d1m0hkrasav4ik/notemanager/AboutAppFragment.java
@@ -2,6 +2,8 @@ package com.d1m0hkrasav4ik.notemanager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,4 +13,10 @@ import android.view.ViewGroup;
 
 public class AboutAppFragment extends Fragment {
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about_app, container,false);
+    }
 }
--- app/src/main/java/com/d1m0hkrasav4ik/notemanager/SettingsFragment.java
+++ app/src/main/java/com/d1m0hkrasav4ik/notemanager/SettingsFragment.java
@@ -2,6 +2,8 @@ package com.d1m0hkrasav4ik.notemanager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,5 +13,11 @@ import android.view.ViewGroup;
 
 public class SettingsFragment extends Fragment {
 
-
+    //указываем макет для настроек
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
 }
